3acb7b7a51964e32e11a18610d58c29f889625eb
rrigs <rrachaelar@gmail.com>
2021-04-06 15:43:22 -0400

    added welcome, movie-card, and user profile components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,29 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component'
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { UpdateProfileComponent } from './update-profile/update-profile.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
-    UserLoginFormComponent
+    UserLoginFormComponent,
+    MovieCardComponent,
+    WelcomePageComponent,
+    UserProfileComponent,
+    UpdateProfileComponent
   ],
   imports: [
     BrowserModule,
@@ -31,8 +47,9 @@ import { UserLoginFormComponent } from './user-login-form/user-login-form.compon
     MatFormFieldModule,
     MatDialogModule,
     MatSnackBarModule,
-    FormsModule
-
+    FormsModule,
+    MatIconModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,98 @@
+import { Component, OnInit } from '@angular/core';
+import { 
+  GetUserService,
+  GetAllMoviesService,
+  RemoveFavoriteMovieService,
+  DeleteUserService
+} from '../fetch-api-data.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { UpdateProfileComponent } from '../update-profile/update-profile.component';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.scss']
+})
+export class UserProfileComponent implements OnInit {
+  user: any = {};
+  movies: any = [];
+  favorites: any = [];
+
+  constructor(
+    public fetchApiData: GetUserService,
+    public fetchApiData2: GetAllMoviesService,
+    public fetchApiData3: RemoveFavoriteMovieService,
+    public fetchApiData4: DeleteUserService,
+    public snackBar: MatSnackBar,
+    public dialog: MatDialog,
+    public router: Router
+  ) { }
+
+//Runs the getUser() function on initialization
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+//Gets the user object from the database and calls the getMovies() function
+  getUser(): void {
+    this.fetchApiData.getUser().subscribe((resp: any) => {
+      this.user = resp;
+      this.getMovies();
+    });
+  }
+
+//Returns a list of all movies from the database and calls the filterFavorites() function
+  getMovies(): void {
+    this.fetchApiData2.getAllMovies().subscribe((resp: any) => {
+      this.movies = resp;
+      this.filterFavorites();
+    });
+  }
+
+  filterFavorites(): void {
+    this.favorites = this.movies.filter((movie: any) =>
+      this.user.FavoriteMovies.includes(movie._id)
+    );
+    return this.favorites;
+  }
+
+// //Removes movie from user's favorites
+//   removeFromFavorites(id: string, title: string): void {
+//     this.fetchApiData3.deleteFavoriteMovie(id).subscribe(() => {
+//       this.snackBar.open(
+//         `${title} has been removed from your Favorites`, 'OK', {
+//           duration: 2000,
+//         }
+//       );
+//       setTimeout(function() {
+//         window.location.reload();
+//       }, 1000);
+//     });
+//   }
+
+//Opens the dialog box where the user can update their information
+  openUpdateProfileDialog(): void {
+    this.dialog.open(UpdateProfileComponent, {
+      width: '280px',
+    });
+  }
+
+//Asks if user is sure about deleting profile
+  deleteProfile(): void {
+    let ok = confirm("Are you sure you want to delete your profile?\nThis action cannot be undone.");
+    if (ok) {
+    this.fetchApiData4.deleteUser().subscribe(() => {
+      console.log('Profile Deleted');
+      localStorage.clear();
+      this.router.navigate(['welcome']); // routes to the 'welcome' view
+      this.snackBar.open('Profile Deleted', 'OK', {
+        duration: 2000,
+      });
+    });
+    } else {
+      window.location.reload();
+    }
+  }
+}
